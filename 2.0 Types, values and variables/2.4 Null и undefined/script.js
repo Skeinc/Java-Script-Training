4f32aa05242712188163323d601f7af8ec3a5ee4
Anufriev Dmitry <skezydeveloper@gmail.com>
2023-06-14 18:16:54 +0300

    Learn null and undefined

---
+++ 2.0 Types, values and variables/2.4 Null и undefined/script.js
@@ -0,0 +1,9 @@
+// null
+let userName = null;
+typeof userName; // => Object: null можно считать особым объектным значением, которое служит признаком "отсутствия объекта"
+
+// undefined
+let userAge;
+typeof userAge; // => undefined
+userName = undefined;
+typeof userName; // => undefined
