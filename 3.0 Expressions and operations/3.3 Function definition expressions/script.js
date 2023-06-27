d216afb642e45ebe0eb10d907487a28b85a62cd8
Anufriev Dmitry <skezydeveloper@gmail.com>
2023-06-27 19:27:57 +0300

    Learn function definition expressions

---
+++ 3.0 Expressions and operations/3.3 Function definition expressions/script.js
@@ -0,0 +1,12 @@
+// Выражения определений функций
+let square = function(x) {
+    return x*x;
+}
+// Объявление функции
+function square(x) {
+    return x*x;
+}
+// Стрелочная функция
+(x) => {
+    return x*x;
+}
